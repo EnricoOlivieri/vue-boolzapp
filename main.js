30d7c7627c19456f7ea4551f9ded69d0bb05623b
Enrico Olivieri <enrico.olivieri@live.it>
2020-11-23 15:23:31 +0100

    css complete

---
+++ main.js
@@ -0,0 +1,16 @@
+const boolzapp = new Vue({
+  el: '#root',
+  data:{
+    mainUser:{
+      name: 'Nome Utente',
+      avatar: ''
+    },
+    contacts: [
+      {
+        name: 'Nome Utente',
+        avatar: '',
+        lastAccess: '',
+      }
+    ]
+  }
+})
